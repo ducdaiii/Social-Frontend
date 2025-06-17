6f3cbdf757d887b2c56ecb6b401b56f4abb416fd
ducdaiii <emhocnguly@gmail.com>
2025-06-17 11:14:39 +0700

    fix 2

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1/",
+  baseUrl: import.meta.env.VITE_API_URL || "https://social-backend-3cv6.onrender.com/api/v1/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     return headers;
